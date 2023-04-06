1d20cd29f0667213fe9e66331d9b1a65eb6c7544
Gabriel Sá <t6w311@gmail.com>
2023-04-06 11:28:46 -0300

    adiciona ID no DTO do cliente

--- src/main/java/com/treinamentoMinsait/empresaFinanceira/DTO/ClienteDTO.java
+++ src/main/java/com/treinamentoMinsait/empresaFinanceira/DTO/ClienteDTO.java
@@ -10,12 +10,14 @@ import lombok.Setter;
 
 @Getter @Setter
 public class ClienteDTO {
+	private Long id;
 	private String nome;
 	private String telefone;
 	private BigDecimal rendaMensal;
 	private Endereco endereco;
 	
 	public ClienteDTO(Cliente cliente) {
+		this.id = cliente.getId();
 		this.nome = cliente.getNome();
 		this.telefone = cliente.getTelefone();
 		this.rendaMensal = cliente.getRendaMensal();
