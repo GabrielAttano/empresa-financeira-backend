33e768b5edeffa5ba1a34179064e2f312925aca8
Gabriel Sá <t6w311@gmail.com>
2023-04-09 14:02:02 -0300

    Adiciona entidade Emprestimo

--- src/main/java/com/treinamentoMinsait/empresaFinanceira/entity/Cliente.java
+++ src/main/java/com/treinamentoMinsait/empresaFinanceira/entity/Cliente.java
@@ -1,12 +1,15 @@
 package com.treinamentoMinsait.empresaFinanceira.entity;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
@@ -31,4 +34,11 @@ public class Cliente {
 	
 	@OneToOne(mappedBy = "cliente", cascade = CascadeType.ALL)
 	@Getter @Setter private Endereco endereco;
+	
+	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+	@Getter private List<Emprestimo> emprestimos = new ArrayList<>();
+	
+	public void setEmprestimos(Emprestimo emprestimo) {
+		this.emprestimos.add(emprestimo);
+	}
 }
--- src/main/java/com/treinamentoMinsait/empresaFinanceira/entity/Emprestimo.java
+++ src/main/java/com/treinamentoMinsait/empresaFinanceira/entity/Emprestimo.java
@@ -1,22 +1,34 @@
 package com.treinamentoMinsait.empresaFinanceira.entity;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Getter;
 import lombok.Setter;
 
-//@Entity
+@Entity
 public class Emprestimo {
-//	@Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    @Getter @Setter private Long id;
-//	
-//	@Getter @Setter private String CPFCliente;
-//	@Getter @Setter private BigDecimal valorInicial;
-//	@Getter @Setter private BigDecimal valorFinal;
-//	
-//	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter private Long id;
+	
+	@Getter @Setter private String CPFCliente;
+	@Getter @Setter private BigDecimal valorInicial;
+	@Getter @Setter private BigDecimal valorFinal;
+	@Getter @Setter @NotNull private LocalDate dataInicial;
+	@Getter @Setter @NotNull private LocalDate dataFinal;
+	
+	@ManyToOne
+	@JoinColumn(name = "cliente_id")
+	@Getter @Setter @JsonIgnore private Cliente cliente;
 }
