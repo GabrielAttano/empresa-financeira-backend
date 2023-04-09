cd77ca115fa1890d76f64a56dbfa72a757484191
Gabriel Sá <t6w311@gmail.com>
2023-04-09 14:02:41 -0300

    Adiciona repositório do Emprestimo

---
+++ src/main/java/com/treinamentoMinsait/empresaFinanceira/repository/EmprestimoRepository.java
@@ -0,0 +1,9 @@
+package com.treinamentoMinsait.empresaFinanceira.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.treinamentoMinsait.empresaFinanceira.entity.Emprestimo;
+
+public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long>{
+
+}
